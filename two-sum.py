b5eed2b5376e5256f4726eb1fb1c222f834c663f
Krishna Sumanth M <krishnasumanthm.1991@gmail.com>
2015-01-08 23:08:47 +0530

    code for two-sum

--- two-sum.py
+++ two-sum.py
@@ -1,18 +1,15 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Wed Jan  7 17:43:23 2015
-
-@author: Krish
-"""
 
 class Solution:
     # @return a tuple, (index1, index2)
     def twoSum(self, num, target):
-        processed = {}
+        temp = {}
         for i in xrange(len(num)):
-            if target-num[i] in processed:
-                return [processed[target-num[i]]+1,i+1]
-            processed[num[i]]=i
+            if target-num[i] in temp:
+                index1 = temp[target-num[i]]+1
+                index2 = i+1
+                return index1,index2
+            temp[num[i]]=i
             
 
                 
