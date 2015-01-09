2f19ae4b8a18c5b717395f1ea9ec7b699587dc97
Krishna Sumanth M <krishnasumanthm.1991@gmail.com>
2015-01-09 18:09:15 +0530

    code for remove-element

--- remove-element.py
+++ remove-element.py
@@ -4,6 +4,11 @@ class Solution:
     # @param    elem    an integer, value need to be removed
     # @return an integer
     def removeElement(self, A, elem):
-        while A.count(elem) != 0:
-            A.remove(A[A.index(elem)])
-        return len(A)
+        j=0
+        i=0
+        while (i<len(A)):
+            if A[i]!=elem:
+                A[j]=A[i]
+                j = j+1
+            i = i+1
+        return j
