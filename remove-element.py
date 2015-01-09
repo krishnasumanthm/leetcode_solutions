eeea61faf3467cdebe756618b5cf5871aa05d12d
Krishna Sumanth M <krishnasumanthm.1991@gmail.com>
2015-01-09 18:12:02 +0530

    code for remove-element

--- remove-element.py
+++ remove-element.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 class Solution:
-    # @param    A       a list of integers
-    # @param    elem    an integer, value need to be removed
+    # @param A a list of integers
+    # @param elem an integer, value need to be removed
     # @return an integer
     def removeElement(self, A, elem):
         j=0
