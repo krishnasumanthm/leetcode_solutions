571f31b6959ba12af24c81e8daa51e2d42b41682
Krishna Sumanth M <krishnasumanthm.1991@gmail.com>
2015-01-17 18:55:58 +0530

    code for plus-one

--- plus-one.py
+++ plus-one.py
@@ -2,17 +2,16 @@ class Solution:
     # @param digits, a list of integer digits
     # @return a list of integer digits
     def plusOne(self, digits):
-        digits[-1] += 1
+        digits[-1] = digits[-1] + 1
         carry = 0
-        for index in xrange(len(digits)-1, -1, -1):
-            digits[index] += carry
-            carry = digits[index] // 10
-            if carry == 0:      
+        for i in xrange(len(digits)-1,-1,-1):
+            digits[i] = digits[i]+carry
+            carry = digits[i]//10
+            if carry == 0:
                 break
-            else:               
-                digits[index] %= 10
+            else:
+                digits[i] = digits[i]%10
         else:
-            
-            digits.insert(0, 1)
+            digits.insert(0,1)
         
         return digits
